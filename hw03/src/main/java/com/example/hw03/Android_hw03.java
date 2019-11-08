384e5724fe9f14c7e4a5d962a023321f2da8913c
love190love <56149448+love190love@users.noreply.github.com>
2019-11-08 20:09:38 +0800

    Update Android_hw03.java

--- hw03/src/main/java/com/example/hw03/Android_hw03.java
+++ hw03/src/main/java/com/example/hw03/Android_hw03.java
@@ -1,4 +1,19 @@
 package com.example.hw03;
 
 public class Android_hw03 {
+     public  static  void main(String[] avrg) {
+        java.util.Scanner scanner= new
+                java.util.Scanner(System.in);
+
+        System.out.println("體重(公斤):");
+        double inp1=scanner.nextInt();
+
+        System.out.println("身高(公分):");
+        double inp2=scanner.nextInt();
+
+        double Hei=inp2/100;
+        double BMI=inp1/(Hei*Hei);
+
+        System.out.println("你 的 BMI 值 : "+BMI);
+    }
 }
